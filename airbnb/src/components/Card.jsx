86793bf84ef59be0182b43fcbaacc9864fb41cc9
Tonnyb <tonnyb@joingladys.com>
2023-02-19 12:27:33 +0300

    adds profile

--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -5,15 +5,28 @@ import star from "../assets/Star.png";
 export default function Card() {
     return (
         <section className="card">
-                <img src={cardImg} alt="cardImg" className="cardImg"/>
-                <div className="cardStats">
-                    <img src={star} alt="star" className="star" />
-                    <span>5.0</span>
-                    <span className="grey">(6).</span>
-                    <span className="grey">USA</span>
-                </div>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><b>From $136</b> / person</p>
+        <div>
+            <img src={cardImg} alt="cardImg" className="cardImg"/>
+            <div className="cardStats">
+                <img src={star} alt="star" className="star" />
+                <span>5.0</span>
+                <span className="grey">(6).</span>
+                <span className="grey">USA</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><b>From $136</b> / person</p>
+        </div>
+        <div>
+            <img src={cardImg} alt="cardImg" className="cardImg"/>
+            <div className="cardStats">
+                <img src={star} alt="star" className="star" />
+                <span>5.0</span>
+                <span className="grey">(6).</span>
+                <span className="grey">USA</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><b>From $136</b> / person</p>
+        </div>
         </section>
         
     );
